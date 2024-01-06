37c7b56ed45dce1496d1bfde53edbb6a63eaa61f
Vleerian R <vleerian@Remordias>
2024-01-05 21:12:39 -0500

    Add Project Files

---
+++ README.md
@@ -0,0 +1,32 @@
+# `zoomies`
+## About
+
+`zoomies` is a script that takes a list of NationStates regions, sorts them by update order, and informs the user when
+the game API reports they have updated.
+
+## Usage
+
+**DO NOT RUN TWO INSTANCES OF `zoomies` AT THE SAME TIME.**
+
+`zoomies` requires a list of regions to trigger on in `trigger_list.txt` - if this file does exist, the program will inform you to come here and figure it out.
+
+Each trigger should be on it's own line, with no additional punctuation.
+
+`zoomies` will prompt you for your main nation - this is used exclusively to identify the current user of the script to NS' admin.
+
+`zoomies` will then demand you provide a poll rate. It is recommended you use a poll speed of 650ms, however you can go higher.
+
+**While 600ms poll speeds are possible, `zoomies` is experimental. You do `zoomies` at your own peril.**
+
+## Acknowledgments
+
+The following people provided key contributions during the initial development process:
+
+* `zoomies` is based on KATT by [Khronion](https://github.com/Khronion)
+* [rootabeta](https://github.com/rootabeta) is the entire reason this exists
+
+## Disclaimer
+
+Any individual using a script-based NationStates API tool is responsible for ensuring it complies with the latest version of the [API Terms of Use](https://www.nationstates.net/pages/api.html#terms). `zoomies` is designed to comply with these rules under reasonable use conditions, but the authors are not responsible for any unintended or erroneous program behavior that breaks these rules.
+
+Never run more than one program that uses the NationStates API at once. Doing so will likely cause your IP address to exceed the API rate limit, which will in turn cause both programs to fail.
---
+++ src/main.rs
@@ -0,0 +1,156 @@
+use inquire::{Text, CustomType, validator::Validation};
+use anyhow::Error;
+use serde::Deserialize;
+use serde_xml_rs::from_str;
+use std::time::Duration;
+use ureq::{Agent, AgentBuilder};
+use std::{
+    fs::File,
+    io::{self, BufRead, BufReader},
+    path::Path,
+    thread::sleep,
+};
+
+pub const LOG_THRESHHOLD : i32 = 30;
+
+macro_rules! log {
+    ($level:ident, $($arg:tt)*) => (println!("[{}] {}", stringify!($level), format!($($arg)*)));
+}
+
+macro_rules! info {
+    ($($arg:tt)*) => (log!(INFO, $($arg)*));
+}
+
+macro_rules! warn {
+    ($($arg:tt)*) => (log!(WARN, $($arg)*));
+}
+
+#[derive(Deserialize)]
+struct Region {
+    id: String,
+    #[serde(alias = "LASTUPDATE")]
+    lastupdate : i32
+}
+
+// Yoinked from https://stackoverflow.com/questions/30801031/read-a-file-and-get-an-array-of-strings
+fn lines_from_file(filename: impl AsRef<Path>) -> io::Result<Vec<String>> {
+    BufReader::new(File::open(filename)?).lines().collect()
+}
+
+fn canonicalize(string: &str) -> String {
+    let mut output = String::from(string);
+    output.make_ascii_lowercase();
+    return str::replace(output.as_str(), " ", "_");
+}
+
+fn get_last_update(agent: &Agent, region: &str) -> Result<Region, Error> {
+    let url = format!(
+        "https://www.nationstates.net/cgi-bin/api.cgi?region={}&q=lastupdate",
+        canonicalize(region)
+    );
+
+    let response = agent
+        .get(&url)
+        .call()?
+        .into_string()?;
+
+    let response : Region = from_str(&response)?;
+    Ok(response)
+}
+
+#[cfg(target_family="windows")]
+fn beep()
+{
+    win_beep::beep_with_hz_and_millis(800, 200);
+}
+
+#[cfg(target_family="unix")]
+fn beep()
+{
+    print!(r"\x07")
+}
+
+fn main() {
+    // The validator used to ensure that the provided poll speed is valid
+    let validator = | input: &u64 | if input < &640 {
+        Ok(Validation::Invalid("Poll speed minimum is 650ms".into()))
+    } else {
+        Ok(Validation::Valid)
+    };
+
+    let main_nation = Text::new("Main Nation: ")
+        .prompt()
+        .unwrap();
+
+    let poll_speed = CustomType::new("Poll Speed (Min 650): ")
+        .with_validator(validator)
+        .prompt()
+        .unwrap();
+
+    let user_agent = format!(
+        "Zoomies/{0} (Developed by nation=Vleerian; In use by nation={1})",
+        env!("CARGO_PKG_VERSION"),
+        main_nation
+    );
+
+    let api_agent: Agent = AgentBuilder::new()
+        .user_agent(&user_agent)
+        .timeout(Duration::from_secs(15))
+        .build();        
+
+    let triggers = lines_from_file("trigger_list.txt")
+        .expect("trigger_list.txt did not exist. Consult README.md for template.");
+
+    let lu_banana = get_last_update(&api_agent, "banana")
+        .unwrap();
+    let lu_wzt = get_last_update(&api_agent, "warzone trinidad")
+        .unwrap();
+    let update_running : bool = lu_banana.lastupdate > lu_wzt.lastupdate;
+
+    // Fetch and sort trigger data
+    let sort_time = (triggers.len() as u64) * poll_speed;
+    info!("Sorting triggers. This will take ~{} seconds.", sort_time);
+    let mut trigger_data : Vec<Region> = Vec::new();
+    for trigger in triggers
+    {
+        sleep(Duration::from_millis(poll_speed));
+        match get_last_update(&api_agent, &trigger)
+        {
+            Ok(region) => {
+                if update_running && lu_banana.lastupdate < region.lastupdate
+                {
+                    warn!("{} has already updated.", region.id);
+                }
+                else
+                {
+                    trigger_data.push(region)
+                }
+            },
+            Err(_) => warn!("Could not fetch Last Update data for {}.", trigger)
+        }
+    }
+    trigger_data.sort_by(| a, b | a.lastupdate.cmp(&b.lastupdate) );
+    info!("Triggers sorted.");
+
+    let banner = "*".repeat(15);
+    for trigger in trigger_data
+    {
+        info!("Waiting for {}", trigger.id);
+        let updated : bool = false;
+        while !updated
+        {
+            sleep(Duration::from_millis(poll_speed));
+            match get_last_update(&api_agent, &trigger.id)
+            {
+                Ok(region) => {
+                    if region.lastupdate != trigger.lastupdate
+                    {
+                        beep();
+                        println!("{}\n{} HAS UPDATED\n{}", banner, region.id, banner);
+                    }
+                }
+                Err(_) => warn!("Fetch failed!")
+            }
+        }
+    }
+}
